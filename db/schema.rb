b3c4266b1fea9ae81143b270e0328b03cab9dfef
roryg11 <Rory.C.Grant@gmail.com>
2014-11-24 13:52:27 -0800

    add email column to people

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124185114) do
+ActiveRecord::Schema.define(version: 20141124214213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141124185114) do
     t.date     "date_of_birth"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
